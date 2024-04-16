dbb3ac1273cd1dfba51746730518c0c4a0e9685a
Dmitri Chebotarev <dmitri.chebotarev@gmail.com>
2024-04-16 22:12:43 +0100

    Fixed sql table

--- sql/1-create-transactions.sql
+++ sql/1-create-transactions.sql
@@ -1,10 +1,14 @@
 CREATE table transactions (
-    id  integer primary key autoincrement not null,
+    id int not null auto_increment,
     amount int not NULL,
     date datetime not null,
     description varchar(255) null,
     payee varchar(255) null,
     address varchar(255) null,
     category varchar(255) null,
+    created_at timestamp null,
+    updated_at timestamp null,
+
+    CONSTRAINT transactions_pk PRIMARY KEY (id)
 )
 
