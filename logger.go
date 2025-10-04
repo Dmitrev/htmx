09ed6e6fa610e10dc024cb4a5f6d28361de3da13
Dmitri <dmitri.chebotarev@gmail.com>
2025-10-04 10:23:14 +0100

    added a logger to do some basic logging

---
+++ logger.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+
+type Color string
+
+const (
+	Reset  Color = "\033[0m"
+	Red    Color = "\033[31m"
+	Green  Color = "\033[32m"
+	Yellow Color = "\033[33m"
+	Blue   Color = "\033[34m"
+)
+
+const tableWidth int = 100
+
+type Logger struct {
+}
+
+func NewLogger() Logger {
+    return Logger{}
+}
+
+func (l *Logger) Sep() {
+    fmt.Println(strings.Repeat("-", tableWidth))
+}
+func (l *Logger) PrintHeading() {
+    l.Sep()
+    fmt.Println("| Method | Uri                                                                                     |")
+    l.Sep()
+}
+
+func (l *Logger) Col(content string, length int, color string) string {
+
+    paddingLeft := 0
+    paddingRight := 0
+
+    if len(content) < length {
+	paddingNeeded := length - len(content)
+	// standard padding
+	paddingLeft++
+	paddingNeeded--
+	paddingRight += paddingNeeded
+	paddingNeeded = 0
+    }
+
+    builder := strings.Builder{}
+    builder.WriteString(color)
+    builder.WriteString(strings.Repeat(" ", paddingLeft))
+    builder.WriteString(content)
+    builder.WriteString(strings.Repeat(" ", paddingRight))
+    builder.WriteString(Reset)
+
+    return builder.String()
+}
+
+func (l *Logger) PrintRequest(method, uri string) {
+
+    color := Reset
+    switch (method) {
+    case "GET": color = Green
+    case "POST": color = Blue
+    case "PUT": color = Yellow
+    case "PATCH": color = Red
+    }
+
+    methodColumnWidth := 8
+    colCount := 2
+    builder := strings.Builder{}
+    builder.WriteString("|")
+    builder.WriteString(l.Col(method, methodColumnWidth, color))
+    builder.WriteString("|")
+    builder.WriteString(l.Col(uri, tableWidth - methodColumnWidth - (colCount + 1), Reset))
+    builder.WriteString("|")
+    fmt.Println(builder.String())
+
+    l.Sep()
+
+    builder.Reset()
+    builder.WriteString("|")
+    builder.WriteString("|")
+    fmt.Println(builder.String())
+}
+
