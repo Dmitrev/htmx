fd70029b6914c4cdfee5b8ec2ecb0ed66b5e728a
Dmitri <dmitri@pop-os.localdomain>
2026-01-31 14:55:21 +0000

    added starting balance to accounts table

--- sql/1-create-accounts.sql
+++ sql/1-create-accounts.sql
@@ -1,8 +1,10 @@
 CREATE table accounts (
     id int not null auto_increment,
     name varchar(255) not null,
+    starting_balance int not null,
     created_at timestamp null,
     updated_at timestamp null,
 
     CONSTRAINT accounts_pk PRIMARY KEY (id)
 )
+
